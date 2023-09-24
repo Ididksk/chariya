94201b6c57cb5b8c3851add39c0168db5b341611
Ididksk <126318627+Ididksk@users.noreply.github.com>
2023-09-24 17:29:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QueenNilu;;;B2xD1K7L#xAZpZIIPBbXAU1aF_F6tizN9x95odamSwiPXJM9w6NI' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
 global.POSTGRESQL_URL = ''
