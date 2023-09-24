41cd08db5804c6f621c081fa694cc77ad3cc0fce
Ididksk <126318627+Ididksk@users.noreply.github.com>
2023-09-24 18:06:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ global.POSTGRESQL_URL = ''
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '94715349194'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'charii_X'
 
 global.TIMEZONE = 'Asia/Colombo' 
 
@@ -41,23 +41,55 @@ global.OPENAI_API_KEY = "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"
 ⚙️ BOT  SETTINGS  ⚙️
 */
 
-global.BOT_NAME = 'ǫᴜᴇᴇɴ ɴɪʟᴜ'
+global.BOT_NAME = '𝘀- 𝘁𝗲𝗳𝗮𝗻 𝗘𝗩𝗢𝗡𝗭  +𝗰 𝗵𝗮 𝗿𝗶𝘁 𝗵𝘇'
  
 global.BOT_OFFLINE = false    //  Always Onlne -  | false offline -True
 
 global.STICKER_NAME = '@Janiya 🤹‍♂️' //sticker
 
-global.FOOTER = 'Queen Nilu 2023 '
+global.FOOTER = '𝘀- 𝘁𝗲𝗳𝗮𝗻 𝗘𝗩𝗢𝗡𝗭  +𝗰 𝗵𝗮 𝗿𝗶𝘁 𝗵𝘇2023 '
 
 global.LANG = 'EN' // Language (EN-  TO english  ,  SI- TO sinhala )
 
 global.PREFIX = '.' // Bot Prefix
 
-global.CAPTION = 'ǫᴜᴇᴇɴ ɴɪʟᴜ  © 2023' // Caption
+global.CAPTION = '𝘀- 𝘁𝗲𝗳𝗮𝗻 𝗘𝗩𝗢𝗡𝗭  +𝗰 𝗵𝗮 𝗿𝗶𝘁 𝗵𝘇  © 2023' // Caption
 
 global.ALIVELOGO = 'https://telegra.ph/file/1e63f0ee90304a12767c7.jpg' // Set Alive Logo link 
 
-global.ALIVE_MESSAGE = 'default' // Set Alive Message
+global.ALIVE_MESSAGE = '{type:alive}
+
+{text:```💃🏻𝙷𝙴𝚈 QUEEN AMDI 𝙱𝙾𝚃 𝙾𝙽𝙻𝙸𝙽𝙴 𝙽𝙾𝚆 💃🏻⚖️```
+▬▬▬▬▬▬▬▬▬▬▬▬
+
+*🔨.𝙳𝙴𝙿𝙻𝙾𝚈𝙴𝙳 𝙱𝚈: ᴄʜᴀʀɪᴛʜᴀ ᴄʜɪʀᴀᴛʜ*
+
+*🔨.𝚃𝚈𝙿𝙴 : .panel 𝚃𝙾 𝙶𝙴𝚃 𝙲𝙸𝙼𝙼𝙰𝙽𝙳 𝙻𝙸𝚂𝚃*
+
+*🔨𝚃𝚈𝙿𝙴: .invite 𝚃𝙾 𝙶𝙴𝚃 𝙸𝙽𝚅𝙸𝚃𝙴 𝙻𝙸𝙽𝙺*
+
+*⚖️𝙱𝙾𝚃 𝙽𝙴𝙼𝙴*:𝚀𝚄𝙴𝙴𝙽 𝙰𝙼𝙳𝙸
+
+*⚙️𝙱𝙾𝚃 𝙾𝚆𝙽𝙴𝚁: 𝙲𝙷𝙰𝚁𝙸𝚃𝙷𝙰 𝙲𝙷𝙸𝚁𝙰𝚃𝙷*
+
+*📱𝙽𝚄𝙼𝙱𝙴𝚁: 94751239272*
+
+*⚖️𝙸𝙽𝚂𝚃𝙰𝙻𝙻 𝙱𝙾𝚃:https://amdaniwasa.com*
+
+*🧑🏻‍💻𝚀𝚄𝙴𝙴𝙽 𝙰𝙼𝙳𝙸 𝙱𝙾𝚃 𝙸𝚂 𝙽𝙾𝚆 𝙾𝙽*:𝙷𝙴𝚁𝙾𝙺𝚄
+
+*🛠️𝙸𝙽𝚂𝚃𝙰𝙻𝙻𝙴𝙳 𝚅𝙴𝚁𝚂𝙸𝙾𝙽*:4.0.6
+
+*🗃️𝙿𝚄𝙱𝙻𝙸𝙲 𝙶𝚁𝙾𝚄𝙿: https://chat.whatsapp.com/DRJlODr5Y1hEXcvENgKBnu*
+
+```💃🏻𝚃𝙷𝙰𝙽𝙺 𝚈𝙾𝚄 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝚀𝚄𝙴𝙴𝙽 𝙰𝙼𝙳𝙸⚖️```}
+
+{url:💃🏻 Github|https://github.com/BlackAmda/QueenAmdi/}
+{url:🎞️ AN Tech YouTube Channel|https://youtube.com/channel/UCZx8U1EU95-Wn9mH4dn15vQ}
+
+{button:*✷𝚂𝚈𝚂𝚃𝙴𝙼 𝚂𝚃𝙰𝚃𝚄𝚂___🤍🫧🍁*|system}
+{button:*✷𝚅𝙴𝚁𝚂𝙸𝙾𝙽___ 🦋🌍*|qaversion}
+{button:*✷𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂___📖🔨*|panel}' // Set Alive Message
 
 global.MAX_SIZE = '400' // Bot Uloading Max size 
 
